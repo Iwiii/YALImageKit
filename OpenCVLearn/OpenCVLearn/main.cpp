7d79e37718cdd96e84e01efa0e9e4df6aff52f86
于玮 <ywcoone@163.com>
2017-12-17 10:39:49 +0800

    OpencvLearn

---
+++ OpenCVLearn/OpenCVLearn/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCVLearn
+//
+//  Created by yuwei on 2017/12/17.
+//  Copyright © 2017年 yuwei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
